c33ce280d6dc4b4995646bf9984a584a15555e9b
mtyszko <michal.tyszko@gmail.com>
2021-02-02 15:50:13 +0100

    navbar mobile

--- app/js/index.js
+++ app/js/index.js
@@ -4,6 +4,8 @@ const navBar = document.querySelector('.navbar');
 
 function handleMenuClick() {
   hamburgerMenu.classList.toggle('menu--active');
+  navBar.classList.toggle('navbar--mobile');
+  navBar.classList.toggle('hide-for-mobile');
 }
 
 hamburgerMenu.addEventListener('click', handleMenuClick);
