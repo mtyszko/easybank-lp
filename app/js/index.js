b8fd389e859b47a7eafd5e34f2d499df747b38b7
mtyszko <michal.tyszko@gmail.com>
2021-02-02 14:24:41 +0100

    header

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,11 @@
-console.log('warczajet');
+const hamburgerMenu = document.querySelector('.menu');
+const navBar = document.querySelector('.navbar');
+// console.log(navBar);
+
+function handleMenuClick() {
+  hamburgerMenu.classList.toggle('menu--active');
+}
+
+hamburgerMenu.addEventListener('click', handleMenuClick);
+
+// handleMenuClick(hamburgerMenu);
